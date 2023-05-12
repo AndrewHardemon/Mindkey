aecbbe4b235b87f460ba85a0f5dd358c8fab4b6b
AndrewHardemon <andrewhardemon@gmail.com>
2023-05-12 05:27:52 -0400

    Added parent/child relationship between blocks

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const createElem = (elem) => document.createElement(elem)
 
 
 let totalBlocks = 0
-const blocks = []
+// const blocks = []
+const blocks = {}
 
 
 const item = getSel(".block")
@@ -13,10 +14,11 @@ const contextElement = getId('context-menu');
 const scope = getSel("body")
 
 class Block {
-  constructor(i){
+  constructor(i, parent){
     this.x = 0
     this.y = 0
     this.i = i
+    this.parentBlock = document.querySelector(".block-"+ parent)
   }
 
   interactBlock(){
@@ -38,26 +40,54 @@ class Block {
     })
   }
 
+  // createLine(){
+  //   const line = createElem('div')
+  //   line.classList.add('line')
+  //   line.dataset.line = this.i
+  //   this.parentBlock.appendChild(line)
+  // }
+
 }
 
 document.addEventListener('click', (e) => {
   if(e.target !== contextElement) {
     contextElement.classList.remove('visible');
   }
+  if(e.target.classList.contains('block')) {
+    // e.target.innerHTML = '<textarea class="block-textarea"></textarea>'
+  }
 })
+
+let temp = ""
 
 contextElement.addEventListener('click', (e) => {
   totalBlocks++
-  
+  console.log(e.target)
+  const parentElem = temp.dataset.block
+  console.log(parentElem)
+
+  const block = new Block(totalBlocks, parentElem)
+  block.interactBlock()
+  // block.createLine()
+  blocks[totalBlocks] = block
+
   const newBlock = createElem('div')
   newBlock.classList.add('block', ('block-'+ totalBlocks))
-  document.body.appendChild(newBlock)
+  newBlock.dataset.block = totalBlocks
+  // const newBlockText = createElem('textarea')
+  // newBlockText.classList.add('block-textarea')
+  // newBlock.appendChild(newBlockText)
   
-  blocks.push(new Block(totalBlocks).interactBlock())
+  block.parentBlock.appendChild(newBlock)
+
+  console.log(blocks)
 })
 
 document.addEventListener('contextmenu', (e) => {
   e.preventDefault();
+  console.log(e.target)
+  temp = e.target
+
   //do something differant context menu
   const { clientX, clientY } = e;
 
@@ -72,7 +102,11 @@ document.addEventListener('contextmenu', (e) => {
 
 function dragAndDrop() {
   getSelAll(".block").forEach((item, i) => {
-    blocks.push(new Block(i).interactBlock())
+    const block = new Block(totalBlocks)
+    block.interactBlock()
+    // block.createLine()
+    blocks[i] = block
+    getSel(".block-"+ i).dataset.block = i
     totalBlocks = i
   })
 }
